1e28dc8c4b24f9110c47349ccc3326262ef5fe92
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2024-11-20 22:26:36 +0530

    template

---
+++ 2024/November/20th_.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int countMinutes()
+  {
+  }
+
+  int takeCharacters(string s, int k)
+  {
+  }
+};
+
+int main()
+{
+  Solution sol;
+
+  string s = "aabaaaacaabc";
+  int k = 2;
+
+  cout << sol.takeCharacters(s, k) << endl;
+
+  return 0;
+}
