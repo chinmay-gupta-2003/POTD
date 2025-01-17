d8b1f3fa60d65865cff4db1bbf2da12d072d589b
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-01-17 11:56:54 +0530

    2683. Neighboring Bitwise XOR

---
+++ 2025/January/17th_2683_Neighboring_Bitwise_XOR.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  bool doesValidArrayExist(vector<int> &derived)
+  {
+    int XOR = 0;
+
+    for (auto it : derived)
+      XOR ^= it;
+
+    return XOR == 0;
+  }
+};
+
+int main()
+{
+  Solution sol;
+  vector<int> derived = {1, 1, 0};
+
+  cout << sol.doesValidArrayExist(derived);
+
+  return 0;
+}
