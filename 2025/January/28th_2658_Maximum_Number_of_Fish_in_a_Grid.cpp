b2d2d4963692222593713859c946808c2cf13a7a
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-01-28 18:43:34 +0530

    2658. Maximum Number of Fish in a Grid DSU

--- 2025/January/28th_2658_Maximum_Number_of_Fish_in_a_Grid.cpp
+++ 2025/January/28th_2658_Maximum_Number_of_Fish_in_a_Grid.cpp
@@ -1,6 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class DisjointSet
+{
+public:
+  vector<int> parent, size;
+
+  DisjointSet(int n)
+  {
+    parent.resize(n);
+    size.resize(n);
+
+    for (int i = 0; i < n; i++)
+    {
+      parent[i] = i;
+      size[i] = 1;
+    }
+  }
+
+  int FindParent(int node)
+  {
+    if (parent[node] == node)
+      return node;
+
+    return parent[node] = FindParent(parent[node]);
+  }
+
+  void UnionBySize(int u, int v)
+  {
+    int parent_u = FindParent(u), parent_v = FindParent(v);
+
+    if (parent_u == parent_v)
+      return;
+
+    if (size[parent_u] > size[parent_v])
+    {
+      parent[parent_v] = parent_u;
+      size[parent_u] += size[parent_v];
+    }
+    else
+    {
+      parent[parent_u] = parent_v;
+      size[parent_v] += size[parent_u];
+    }
+  }
+
+  void SetInitialSize(int node, int n)
+  {
+    size[node] = n;
+  }
+};
+
 class Solution
 {
 public:
@@ -21,7 +71,7 @@ public:
     }
   }
 
-  int findMaxFish(vector<vector<int>> &grid)
+  int findMaxFishDFS(vector<vector<int>> &grid)
   {
     int m = grid.size(), n = grid[0].size();
     vector<vector<int>> vis(m, vector<int>(n, 0));
@@ -44,6 +94,49 @@ public:
 
     return result;
   }
+
+  int findMaxFishDSU(vector<vector<int>> &grid)
+  {
+    int m = grid.size(), n = grid[0].size();
+
+    DisjointSet DS(m * n);
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+        DS.SetInitialSize(i * n + j, grid[i][j]);
+    }
+
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (grid[i][j])
+        {
+          for (auto coords : directions)
+          {
+            int n_i = i + coords[0];
+            int n_j = j + coords[1];
+
+            if (n_i >= 0 && n_i < m && n_j >= 0 && n_j < n && grid[n_i][n_j])
+              DS.UnionBySize(i * n + j, n_i * n + n_j);
+          }
+        }
+      }
+    }
+
+    int result = 0;
+
+    for (int i = 0; i < m * n; i++)
+    {
+      if (DS.parent[i] == i)
+        result = max(result, DS.size[i]);
+    }
+
+    return result;
+  }
 };
 
 int main()
@@ -51,7 +144,8 @@ int main()
   Solution sol;
   vector<vector<int>> grid = {{0, 2, 1, 0}, {4, 0, 0, 3}, {1, 0, 0, 4}};
 
-  cout << sol.findMaxFish(grid);
+  cout << sol.findMaxFishDSU(grid) << endl;
+  cout << sol.findMaxFishDFS(grid) << endl;
 
   return 0;
 }
