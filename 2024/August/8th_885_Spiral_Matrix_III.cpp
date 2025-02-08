8e570aa4a058760b4267858b085a269a8b93d4f4
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-02-08 23:58:23 +0530

    885. Spiral Matrix III v2

--- 2024/August/8th_885_Spiral_Matrix_III.cpp
+++ 2024/August/8th_885_Spiral_Matrix_III.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class Solution_1
 {
 public:
   bool isValid(int rows, int cols, int row, int col)
@@ -75,14 +75,56 @@ public:
   }
 };
 
+class Solution_2
+{
+public:
+  vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart)
+  {
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    int step = 0;
+    int dir = 0;
+
+    vector<vector<int>> result;
+
+    result.push_back({rStart, cStart});
+
+    while (result.size() < rows * cols)
+    {
+      if (dir == 0 || dir == 2)
+        step++;
+
+      for (int i = 0; i < step; i++)
+      {
+        rStart += directions[dir][0];
+        cStart += directions[dir][1];
+
+        if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols)
+          result.push_back({rStart, cStart});
+      }
+
+      dir = (dir + 1) % 4;
+    }
+
+    return result;
+  }
+};
+
 int main()
 {
-  Solution sol;
+  Solution_1 sol1;
   int rows = 5, cols = 6, rStart = 1, cStart = 4;
 
-  auto ans = sol.spiralMatrixIII(rows, cols, rStart, cStart);
+  auto ans1 = sol1.spiralMatrixIII(rows, cols, rStart, cStart);
 
-  for (auto it : ans)
+  for (auto it : ans1)
+    cout << it[0] << " , " << it[1] << endl;
+
+  Solution_2 sol2;
+
+  auto ans2 = sol2.spiralMatrixIII(rows, cols, rStart, cStart);
+
+  for (auto it : ans2)
     cout << it[0] << " , " << it[1] << endl;
 
   return 0;
