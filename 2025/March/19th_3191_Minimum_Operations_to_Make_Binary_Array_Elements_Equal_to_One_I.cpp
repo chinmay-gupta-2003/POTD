cf2e8eff94fd7c94b0905a3c1a22595070958877
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-03-19 22:13:45 +0530

    3191. Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ 2025/March/19th_3191_Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int minOperationsBrute(vector<int> &nums)
+  {
+    int n = nums.size();
+
+    int result = 0;
+
+    for (int i = 0; i < n - 2; i++)
+    {
+      if (nums[i])
+        continue;
+
+      result++;
+
+      nums[i] ^= 1;
+      nums[i + 1] ^= 1;
+      nums[i + 2] ^= 1;
+    }
+
+    if (!nums[n - 1] || !nums[n - 2])
+      return -1;
+
+    return result;
+  }
+};
+
+int main()
+{
+  Solution sol;
+  vector<int> nums = {0, 1, 1, 1, 0, 0};
+
+  cout << sol.minOperationsBrute(nums) << endl;
+
+  return 0;
+}
