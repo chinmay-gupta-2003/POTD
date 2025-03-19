7eec58fe08948f3868d4452e4af42b1ddb4a44f8
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-03-19 22:33:52 +0530

    703. Kth Largest Element in a Stream

---
+++ 2024/August/12th_703_Kth_Largest_Element_in_a_Stream.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class KthLargest
+{
+  priority_queue<int, vector<int>, greater<int>> pq;
+  int k;
+
+public:
+  KthLargest(int k, vector<int> &nums)
+  {
+    this->k = k;
+
+    for (int num : nums)
+      add(num);
+  }
+
+  int add(int val)
+  {
+    if (pq.size() < k || pq.top() < val)
+      pq.push(val);
+
+    if (pq.size() > k)
+      pq.pop();
+
+    return pq.top();
+  }
+};
+
+int main()
+{
+  return 0;
+}
