19c5d0383a83a0989ec508ce958cf23b5823c469
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-05-21 11:02:59 +0530

    remove

--- 2025/May/21st_73_Set_Matrix_Zeroes.cpp
+++ 2025/May/21st_73_Set_Matrix_Zeroes.cpp
@@ -31,30 +31,6 @@ public:
       }
     }
   }
-
-  void setZeroesOptimal(vector<vector<int>> &matrix)
-  {
-    int m = matrix.size(), n = matrix[0].size();
-
-    int zeroRow = 0;
-
-    for (int i = 0; i < m; i++)
-    {
-      for (int j = 0; j < n; j++)
-      {
-        if (matrix[i][j] == 0)
-        {
-        }
-      }
-    }
-
-    for (int i = 0; i < m; i++)
-    {
-      for (int j = 0; j < n; j++)
-      {
-      }
-    }
-  }
 };
 
 int main()
