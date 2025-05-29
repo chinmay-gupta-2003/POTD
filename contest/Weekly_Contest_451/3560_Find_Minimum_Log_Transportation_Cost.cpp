d04277921c7e5363e91a8dc07eab98ea41e41fdf
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2025-05-29 15:36:51 +0530

    3560. Find Minimum Log Transportation Cost

---
+++ contest/Weekly_Contest_451/3560_Find_Minimum_Log_Transportation_Cost.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  long long getLogCost(int l, int k)
+  {
+    long long cost = LONG_LONG_MAX;
+
+    if (l <= k)
+      return 0;
+
+    for (long long len1 = 0; len1 <= k; len1++)
+    {
+      long long len2 = l - len1;
+
+      if (len2 <= k)
+        cost = min(cost, len1 * len2);
+    }
+
+    return cost;
+  }
+
+  long long minCuttingCostBrute(int n, int m, int k)
+  {
+    return getLogCost(n, k) + getLogCost(m, k);
+  }
+};
+
+int main()
+{
+  Solution sol;
+  int n = 49898, m = 109372, k = 62703;
+
+  cout << sol.minCuttingCostBrute(n, m, k);
+
+  return 0;
+}
